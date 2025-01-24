f44184ea9855e5dee91740810abbf7f30ae43a7d
Marco Bevivino (MSC Technology Italia) <marco.bevivino@msc.com>
2025-01-24 13:58:01 +0100

    controller done

--- Reactivities/API/Controllers/ActivitiesController.cs
+++ Reactivities/API/Controllers/ActivitiesController.cs
@@ -2,20 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers 
 {    
-    [ApiController]
-    [Route("[controller]")]
+
     public class ActivitiesController: BaseApiController
 
     {
-        private readonly DataContext context;
+        private readonly DataContext _context;
+
         public ActivitiesController(DataContext context)
         {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities() 
+        { 
+            return await _context.Activities.ToListAsync<Activity>();
             
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> getActivity(Guid id)
+        { 
+            return await _context.Activities.FindAsync(id);
+        }
+
+
     }
 }
